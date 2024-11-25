df6a1338fe57ddb50a6ae984e91bf65204f0cee1
Marcel Suter <marcel.suter@bzz.ch>
2024-11-25 10:28:27 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 14.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+from authenticate import login
+from lottery import create_ticket
+from menu import select_menu
+from money import transfer_money
+
+
+def main():
+    person = login()
+    item = ''
+    while item != 'Z':
+        item = select_menu()
+        if item == 'A':
+            transfer_money(person)
+        elif item == 'B':
+            create_ticket(person)
 
 
 if __name__ == '__main__':
-    calculate()
+    main()
